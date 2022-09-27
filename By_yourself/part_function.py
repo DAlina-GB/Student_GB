e1ea152b73cddca38015f70e65b82835c8c2a662
Alina Dainakova <alinoskar@gmail.com>
2022-09-27 19:09:30 +0300

    1

---
+++ By_yourself/part_function.py
@@ -0,0 +1,16 @@
+# фунция которая переводит мили в коллометры
+
+# def Converts(miles):
+#     km = miles * 1.609
+#     print (km)
+    
+# miles = int (input ("Input miles "))
+# print = Converts (miles)
+
+miles = 12
+def Converts(miles):
+     km = miles * 1.609
+    #  print (km)
+
+
+print = Converts(miles)
