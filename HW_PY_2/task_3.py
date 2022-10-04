0dd7188752f26cb2185a0f87611c1dea031a4362
Alina Dainakova <alinoskar@gmail.com>
2022-10-04 11:44:58 +0300

    done

--- HW_PY_2/task_3.py
+++ HW_PY_2/task_3.py
@@ -1,3 +1,27 @@
 # Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 # *Пример:*
 # - Для n = 6: {1: 2, 2: 2.25, 3: 2.37, 4: 2.44, 5: 2.49, 6: 2.52}
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть целое ")
+    return number
+
+num = InputNumbers("Введите число n: ")
+elements = []
+for i in range(num):
+    elements.append((1 + 1/(i + 1)) ** (i + 1))
+
+print(elements)
+
+sum1 = 0
+for i in range(num):
+    sum1 += elements[i]
+
+print(sum1)
+
--- HW_PY_2/task_4.py
+++ HW_PY_2/task_4.py
@@ -1,1 +1,14 @@
 # Реализуйте алгоритм перемешивания списка.
+from random import randint
+import math 
+
+nums = [1111, 23, 348889, 4, 55, 67777777, 0.8, -0.8]
+print(nums)
+
+for i in range(len(nums)):
+    temp = nums[i]
+    new_index = randint(0, len(nums)-1)
+    nums[i] = nums[new_index]
+    nums[new_index] = temp
+
+print(nums)
