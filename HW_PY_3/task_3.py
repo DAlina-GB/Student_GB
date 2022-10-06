bb1536de0207ef09a96bb8006b550b422c0a031f
Alina Dainakova <alinoskar@gmail.com>
2022-10-06 22:07:55 +0300

    task is done

--- HW_PY_3/task_3.py
+++ HW_PY_3/task_3.py
@@ -1,5 +1,43 @@
-# Задайте список из вещественных чисел. 
-# Напишите программу, которая найдёт разницу между максимальным 
+# Задайте список из вещественных чисел.
+# Напишите программу, которая найдёт разницу между максимальным
 # и минимальным значением дробной части элементов.
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+a = [1.1, 1.2, 3.1, 5.10, 10.01]
+
+
+def average(n):
+    max = 0
+    min = 1
+    for i in n:
+        temp = round(i % 1, 3)
+        if temp > max:
+            max = temp
+        elif temp < min:
+            min = temp
+    print(f"max {max} min {min}")
+    res = max - min
+    print("Разница между min и max", res)
+
+
+average(a)
+
+# Решение с семинара:
+# input_list = input("Введите список чисел, разделенных пробелом: ").split()
+# fract_list = []
+# i_max = 0
+# i_min = 0
+# for i in range(len(input_list)):
+# if float(input_list[i]) % 1 != 0:
+# j = round((float(input_list[i]) % 1), 2)
+# fract_list.append(j)
+# for i in range(len(fract_list)):
+# if fract_list[i] > fract_list[i_max]:
+# i_max = i
+# if fract_list[i] < fract_list[i_min]:
+# i_min = i
+# res = float(fract_list[i_max]) - float(fract_list[i_min])
+# print("Список исходный значений: ", input_list)
+# #print("Список дробных частей исходный значений:\n",fract_list)
+# print("Разница между максимальным и минимальным значением дробной части элементов: ", res)
--- HW_PY_3/task_5.py
+++ HW_PY_3/task_5.py
@@ -3,3 +3,46 @@
 # - для k = 8 список будет выглядеть так:
 #  [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 #  [Негафибоначчи]
+
+def fibo(n):
+    fibo_list = list()
+    fibo_list.append(0)
+    fibo_list.append(1)
+
+    for i in range(2, n + 1):
+        fibo_list.append(fibo_list[i - 1] + fibo_list[i - 2])
+
+    fibo_list.insert(0, 1)
+    fibo_list.insert(0, -1)
+
+    for i in range(0, n - 2):
+        fibo_list.insert(0, (fibo_list[1]) - (fibo_list[0]))
+    return fibo_list
+
+
+x = fibo(8)
+print(x)
+
+
+# Решения с семинара
+# Вариант 1
+# def nega_fibonacci(n):
+# list_negafibonacci = [0]
+# if n == 0:
+# return list_negafibonacci
+# elif n == 1:
+# list_negafibonacci = [1, 0, 1]
+# return list_negafibonacci
+# else:
+# list_negafibonacci = [-1, 1, 0, 1, 1]
+# fib1 = fib2 = 1
+# for i in range(2, n):
+# fib1, fib2 = fib2, fib1 + fib2 # fib1 приравнивается к fib2, fib2 приравнивается к fib1 + fib2
+# list_negafibonacci.append(fib2)
+# list_negafibonacci.insert(0, (fib2 * (-1) ** i))
+# return list_negafibonacci
+
+
+# n = int(input("Введите число: "))
+# print((f"Список чисел (нега)Фибоначчи для k = {n}: {nega_fibonacci(n)}"))
+
