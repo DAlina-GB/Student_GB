4ca4aef4b615b57ff5f39d225e59d1432e3e9a47
Alina Dainakova <alinoskar@gmail.com>
2022-11-11 01:15:07 +0300

    tictactoe_2

--- python_9/main.py
+++ python_9/main.py
@@ -1,8 +1,32 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import os
+import sys
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+
 from bot_commands import *
+import strings as st
 
 updater = Updater('5415981853:AAE4UXhwdqxW8_8vHn5DFIkjiMgxv78uiNw')
+
+
+def getToken():
+    token = '5551618618:AAEIzUbEK5BFTQtPr9C75v_GQIDrc3K3a58'
+    if os.path.isfile(st.BOT_TOKEN_FILENAME):
+        f = open(st.BOT_TOKEN_FILENAME, "r")
+        token = f.read()
+        f.close()
+    else:
+        print("Пожалуйста, создайте в папке проекта файл 'token.txt' и поместите туда токен для работы телеграм бота  и запустите скрипт заново")
+        sys.exit()  # завершить работу скрипта
+    return token
+
+def help_command(update, _):
+    update.message.reply_text(st.ANSW_HELP)
+
+
+if __name__ == '__main__':
+    updater = Updater(getToken())  # получения токена из файла 'token.txt' и инициализация updater
 
 updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
@@ -10,6 +34,10 @@ updater.dispatcher.add_handler(CommandHandler('help', help_command))
 updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
 updater.dispatcher.add_handler(CommandHandler('w_d', w_d_command))
 updater.dispatcher.add_handler(CommandHandler('ran', ran_command))
+updater.dispatcher.add_handler(CommandHandler('start', newGame))
+updater.dispatcher.add_handler(CommandHandler('new_game', newGame))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, help_command))  # обработчик на любое текстовое сообщение
+updater.dispatcher.add_handler(CallbackQueryHandler(button))  # добавление обработчика на CallBack кнопки
 
 updater.start_polling()
 updater.idle()
